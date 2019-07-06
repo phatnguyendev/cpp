43f8c841986749eae9b5483385ee91f4411046fb
phatnguyendev <50161390+phatnguyendev@users.noreply.github.com>
2019-07-07 01:09:18 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  ((localStorage.getItem('mode') || 'light') === 'dark') ? document.querySelector('body').classList.add('dark'); document.getElementById("mode").innerHTML="Dark mode"; : document.querySelector('body').classList.remove('dark');document.getElementById("mode").innerHTML="Light mode"
+  ((localStorage.getItem('mode') || 'light') === 'dark') ? document.querySelector('body').classList.add('dark'); document.getElementById("mode").innerHTML="Dark mode" : document.querySelector('body').classList.remove('dark');document.getElementById("mode").innerHTML="Light mode"
 })
