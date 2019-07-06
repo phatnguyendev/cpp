b29e812f0a659ec1b94ba47031b271830cbcdb69
phatnguyendev <50161390+phatnguyendev@users.noreply.github.com>
2019-07-07 01:41:31 +0700

    final

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  ((localStorage.getItem('mode') || 'light') === 'light') ? document.querySelector('body').classList.remove('dark') : document.querySelector('body').classList.add('dark')
+  var mode = ((localStorage.getItem('mode') || 'light') === 'light') ? 'light' : 'dark';  
+  if(mode === 'dark')
+    this.innerHTML = "Light mode";
+  else
+    this.innerHTML = "Dark mode";
 })
 
 document.getElementById("mode").addEventListener("click", changemode);
