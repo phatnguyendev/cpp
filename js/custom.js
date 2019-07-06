cb90c9d69832990a43449e3f5e8ee9dc6e1a0f3d
phatnguyendev <50161390+phatnguyendev@users.noreply.github.com>
2019-07-07 01:35:28 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
   ((localStorage.getItem('mode') || 'light') === 'light') ? document.querySelector('body').classList.remove('dark') : document.querySelector('body').classList.add('dark')
 })
 
-document.getElementById("mode").addEventListener("click", {
+document.getElementById("mode").addEventListener("click", changemode);
+
+function changemode() {
   var mode = ((localStorage.getItem('mode') || 'light') === 'light') ? 'light' : 'dark';  
   if(mode === 'dark')
     this.innerHTML = "Light mode";
   else
     this.innerHTML = "Dark mode";
-});
+}
